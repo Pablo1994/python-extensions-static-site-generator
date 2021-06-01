c635b35df483301da1fd867127747ca79297bceb
Pablo Arias <JuanPablo.Arias@team.neustar>
2021-06-01 09:41:43 -0600

    +=

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -11,3 +11,9 @@ total_written = 0
 def start_build():
     global start_time 
     start_time = time.time()
+
+
+@hooks.register("written")
+def written():
+    global total_written
+    total_written += 1
